676276d8f163825d5d05d001f288b65ca40ca67a
aliceh0155 <hofverbergalice@gmail.com>
2024-09-13 15:05:50 +0200

    first commit

---
+++ src/main/java/com/alice/wsprojektuppgift/WsProjektuppgiftApplication.java
@@ -0,0 +1,13 @@
+package com.alice.wsprojektuppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjektuppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsProjektuppgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alice/wsprojektuppgift/WsProjektuppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alice.wsprojektuppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsProjektuppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
