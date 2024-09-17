f48dbdb32e433461767aa27a46952176fc0f8bc1
aliceh0155 <hofverbergalice@gmail.com>
2024-09-17 22:05:20 +0200

    making model

---
+++ src/main/java/com/alice/wsprojektuppgift/model/CharacterModel.java
@@ -0,0 +1,21 @@
+package com.alice.wsprojektuppgift.model;
+
+
+public class CharacterModel {
+
+    private String id;
+    private String name;
+    private String species;
+    private String gender;
+    private String house;
+    private String dateOfBirth;
+    private int yearOfBirth;
+    private boolean wizard;
+    private String ancestry;
+    private String eyeColour;
+    private String hairColour;
+    private Wand wand;
+    private boolean hogwartsStudent;
+    private boolean alive;
+
+}
---
+++ src/main/java/com/alice/wsprojektuppgift/model/Wand.java
@@ -0,0 +1,32 @@
+package com.alice.wsprojektuppgift.model;
+
+public class Wand {
+    private String wood;
+    private String core;
+    private double length;
+
+    public String getWood() {
+        return wood;
+    }
+
+    public void setWood(String wood) {
+        this.wood = wood;
+    }
+
+    public String getCore() {
+        return core;
+    }
+
+    public void setCore(String core) {
+        this.core = core;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+}
