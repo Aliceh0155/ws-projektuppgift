1d3c19882f903d5dff730d211d6d389d7c0ecf65
aliceh0155 <hofverbergalice@gmail.com>
2024-11-06 15:05:28 +0100

    Add: Starting to implement security/roles

--- src/main/java/com/alice/wsprojektuppgift/controller/CharacterController.java
+++ src/main/java/com/alice/wsprojektuppgift/controller/CharacterController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
 import java.util.List;
 
 @RestController
